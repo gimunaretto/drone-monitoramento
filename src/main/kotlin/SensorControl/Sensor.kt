d767a606556cd6ec7da21b79f4cd14a907c2cf72
gimunaretto <61035475+gimunaretto@users.noreply.github.com>
2022-12-05 22:11:49 -0300

    Criação de projeto de Drone de monitoramento com readme

--- src/main/kotlin/SensorControl/Sensor.kt
+++
@@ -1,7 +0,0 @@
-package SensorControl
-
-class Sensor {
-    fun generate(): Pair<Int, Int> =
-        (-25..0).random() to (0..10).random()  // ##MOCK
-        //(-25..0).random() to (0..100).random()
-}
